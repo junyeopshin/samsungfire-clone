670e6b8d4ec6b47f49b80fee70d701a8edf9a2d2
junyeopshin <macsin77@gmail.com>
2025-06-09 13:04:03 +0900

    design: desktop 화면 퍼블리싱 완료

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+const footerMenuList = document.querySelectorAll(
+  ".footer-top .inner > ul > li"
+);
+const footerSubmenuList = document.querySelectorAll(
+  ".footer-top .inner .submenu"
+);
+
+footerMenuList.forEach((menu) => {
+  menu.addEventListener("mouseenter", () => {
+    footerSubmenuList.forEach((submenu) => {
+      submenu.style.display = "block";
+    });
+  });
+  menu.addEventListener("mouseleave", () => {
+    footerSubmenuList.forEach((submenu) => {
+      submenu.style.display = "none";
+    });
+  });
+});
